1523dbe4a51511e5ac8a3b0093795e9059426e9f
Alexander <alzahar@ukr.net>
2024-02-05 17:06:09 +0200

    add: all tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,16 @@
-function checkForSpam(message) {
-    const spam = ['spam', 'sale'];
-    const lowerMessage = message.toLowerCase();
-    for (let i = 0; i < spam.length; i++) {
-        if (lowerMessage.indexOf(spam[i]) !== -1) {
-            return true;
+function filterArray(numbers, value) {
+    const resultArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            resultArray.push(number);
         }
     }
 
-    return false;
+    return resultArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
